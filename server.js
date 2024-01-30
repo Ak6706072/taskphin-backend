340f2ba6fb4b76d19aa0606c05b4bde9eaf587ec
Ashish Kumar <ashishkumar@Ashishs-MacBook-Air.local>
2024-01-31 01:57:25 +0530

    added db connection

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import client from "./src/db/index.js";
+
+const app = express();
+dotenv.config({ path: ".env" });
+const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    app: "is running",
+  });
+});
+
+app.get("/create", async (req, res) => {
+  await client.query(`CREATE TABLE Candidate { 
+    Id SERIAL PRIMARY KEY, 
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(500),
+    City varchar(255)}`);
+});
+
+app.listen(port, () => {
+  console.log("server is running on", port);
+});
+
+client.connect();
--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,21 @@
-import { Client } from "pg";
+import pg from "pg";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const client = new Client({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: `${__dirname}/../../.env` });
+
+console.log("dsadsad", process.env.DB_HOST);
+const client = new pg.Client({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DATABASE_NAME,
   user: process.env.USER_NAME,
   password: process.env.DB_PASSWORD,
+  ssl: true,
 });
 
 export default client;
