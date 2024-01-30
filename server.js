b11329ef760d3e20810abb49d9ce06538bdfc2bc
Ashish Kumar <ashishkumar@Ashishs-MacBook-Air.local>
2024-01-30 21:29:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    app: "is running",
+  });
+});
+
+app.listen(port, () => {
+  console.log("server is running on", port);
+});
